13bd9249a6688d7b28eeb275ea9fb69af712660c
Plaksa Anastasia <plaksanasta13@gmail.com>
2023-08-23 20:19:48 +0300

    Add switch and case

--- Jokes/Program.cs
+++ Jokes/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Выберите категорию анекдотов из предложенных: Автомобили и транспорт, Брак, Теща, Психушка\n");
+string category = Console.ReadLine();
+
+switch(category)
+{
+    case "Автомобили и транспорт":
+        Console.WriteLine("1\n");
+        break;
+    case "Брак":
+        Console.WriteLine("2\n");
+        break;
+    case "Теща":
+        Console.WriteLine("3\n");
+        break;
+    case "Психушка":
+        Console.WriteLine("4\n");
+        break;
+    default:
+        Console.WriteLine("\nКатегория введена неправильно, либо данной категории не существует. Повторите попытку еще раз\n");
+        break;
+}
